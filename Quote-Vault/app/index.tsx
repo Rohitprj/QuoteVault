93352d921f274697c8475c7a1c773986ee4930fc
Rohit kumar <rohitprj.1282@gmail.com>
2026-01-14 18:43:25 +0530

    go

--- Quote-Vault/app/index.tsx
+++ Quote-Vault/app/index.tsx
@@ -3,5 +3,5 @@ import { Redirect } from "expo-router";
 export default function Index() {
   // Redirect to tabs (home) on app start
   // Change this to '/sign-in' if you want authentication first
-  return <Redirect href="/(tabs)/home" />;
+  return <Redirect href="/sign-in" />;
 }
--- Quote-Vault/babel.config.js
+++ Quote-Vault/babel.config.js
@@ -1,22 +1,20 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
-      '@babel/plugin-transform-react-jsx',
-      'react-native-reanimated/plugin',
+      "react-native-reanimated/plugin",
       [
-        'module:react-native-dotenv',
+        "module:react-native-dotenv",
         {
-          moduleName: '@env',
-          path: '.env',
+          moduleName: "@env",
+          path: ".env",
           blacklist: null,
           whitelist: null,
           safe: false,
           allowUndefined: true,
         },
       ],
-      require.resolve("expo-router/babel"),
     ],
   };
 };

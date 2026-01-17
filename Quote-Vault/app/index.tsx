547ea00e45fa09f916239f71b6e5b8d0dac4736e
Rohit kumar <rohitprj.1282@gmail.com>
2026-01-17 20:38:09 +0530

    go

--- Quote-Vault/app/index.tsx
+++ Quote-Vault/app/index.tsx
@@ -1,7 +1,8 @@
-import { Redirect } from "expo-router";
+import { Redirect, Stack } from "expo-router";
 
 export default function Index() {
-  // Redirect to tabs (home) on app start
+  // Redirect to tabs (home) on app startr
   // Change this to '/sign-in' if you want authentication first
-  return <Redirect href="/sign-in" />;
+  // return <Redirect href="/sign-in" />;
+  return <Stack />;
 }

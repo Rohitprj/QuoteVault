af616f964c4d6d78681e357d4e358a1982343d3a
Rohit kumar <rohitprj.1282@gmail.com>
2026-01-17 18:53:12 +0530

    not fixed yet

--- Quote-Vault/babel.config.js
+++ Quote-Vault/babel.config.js
@@ -1,8 +1,31 @@
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: ["babel-preset-expo"],
+//     plugins: [
+//       [
+//         "module:react-native-dotenv",
+//         {
+//           moduleName: "@env",
+//           path: ".env",
+//           blacklist: null,
+//           whitelist: null,
+//           safe: false,
+//           allowUndefined: true,
+//         },
+//       ],
+//       "react-native-reanimated/plugin",
+//     ],
+//   };
+// };
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
     plugins: [
+      // "@babel/plugin-transform-react-jsx",
+      "react-native-reanimated/plugin",
       [
         "module:react-native-dotenv",
         {
@@ -14,7 +37,7 @@ module.exports = function (api) {
           allowUndefined: true,
         },
       ],
-      "react-native-reanimated/plugin",
+      require.resolve("expo-router/babel"),
     ],
   };
 };
